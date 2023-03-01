26ab56213b65b677f05ee2e5c88f1e92c17b4441
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2023-03-01 01:31:19 -0500

    Update SaleInfo.tsx

--- components/SaleInfo/SaleInfo.tsx
+++ components/SaleInfo/SaleInfo.tsx
@@ -13,6 +13,7 @@ import {
 import {
   MARKETPLACE_ADDRESS,
   NFT_COLLECTION_ADDRESS,
+  NFT_COLLECTION_ADDRESS_2,
 } from "../../const/contractAddresses";
 import { useRouter } from "next/router";
 import toast, { Toaster } from "react-hot-toast";
@@ -50,7 +51,7 @@ export default function SaleInfo({ nft }: Props) {
   // useContract is a React hook that returns an object with the contract key.
   // The value of the contract key is an instance of an NFT_COLLECTION on the blockchain.
   // This instance is created from the contract address (NFT_COLLECTION_ADDRESS)
-  const { contract: nftCollection } = useContract(NFT_COLLECTION_ADDRESS);
+  const { contract: nftCollection } = useContract(NFT_COLLECTION_ADDRESS, NFT_COLLECTION_ADDRESS_2);
 
   // Hook provides an async function to create a new auction listing
   const { mutateAsync: createAuctionListing } =
@@ -68,6 +69,7 @@ export default function SaleInfo({ nft }: Props) {
     useForm<AuctionFormData>({
       defaultValues: {
         nftContractAddress: NFT_COLLECTION_ADDRESS,
+        nftContractAddress: NFT_COLLECTION_ADDRESS_2,
         tokenId: nft.metadata.id,
         startDate: new Date(),
         endDate: new Date(),
@@ -110,6 +112,7 @@ export default function SaleInfo({ nft }: Props) {
     useForm<DirectFormData>({
       defaultValues: {
         nftContractAddress: NFT_COLLECTION_ADDRESS,
+        nftContractAddress: NFT_COLLECTION_ADDRESS_2,
         tokenId: nft.metadata.id,
         startDate: new Date(),
         endDate: new Date(),
@@ -223,7 +226,7 @@ export default function SaleInfo({ nft }: Props) {
                 position: "bottom-center",
               });
               router.push(
-                `/token/${NFT_COLLECTION_ADDRESS}/${nft.metadata.id}`
+                `/token/${NFT_COLLECTION_ADDRESS, NFT_COLLECTION_ADDRESS_2}/${nft.metadata.id}`
               );
             }}
           >
@@ -298,7 +301,7 @@ export default function SaleInfo({ nft }: Props) {
                 position: "bottom-center",
               });
               router.push(
-                `/token/${NFT_COLLECTION_ADDRESS}/${nft.metadata.id}`
+                `/token/${NFT_COLLECTION_ADDRESS, NFT_COLLECTION_ADDRESS_2}/${nft.metadata.id}`
               );
             }}
           >
