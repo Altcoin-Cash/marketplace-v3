0b561dc220aa8b612e10d1f3bab3432be6ed6472
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2023-03-01 01:33:43 -0500

    Update NFTGrid.tsx

--- components/NFT/NFTGrid.tsx
+++ components/NFT/NFTGrid.tsx
@@ -2,6 +2,7 @@ import type { NFT as NFTType } from "@thirdweb-dev/sdk";
 import Link from "next/link";
 import React from "react";
 import { NFT_COLLECTION_ADDRESS } from "../../const/contractAddresses";
+import { NFT_COLLECTION_ADDRESS_2 } from "../../const/contractAddresses";
 import Skeleton from "../Skeleton/Skeleton";
 import NFT from "./NFT";
 import styles from "../../styles/Buy.module.css";
@@ -31,7 +32,7 @@ export default function NFTGrid({
         data.map((nft) =>
           !overrideOnclickBehavior ? (
             <Link
-              href={`/token/${NFT_COLLECTION_ADDRESS}/${nft.metadata.id}`}
+              href={`/token/${NFT_COLLECTION_ADDRESS, NFT_COLLECTION_ADDRESS_2}/${nft.metadata.id}`}
               key={nft.metadata.id}
               className={styles.nftContainer}
             >
