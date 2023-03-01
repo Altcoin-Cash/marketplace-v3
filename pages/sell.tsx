95ff8dfef82e0c4114542b3ec8366d892ff8fc07
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2023-03-01 01:58:46 -0500

    Update sell.tsx

--- pages/sell.tsx
+++ pages/sell.tsx
@@ -8,7 +8,6 @@ import React, { useState } from "react";
 import Container from "../components/Container/Container";
 import NFTGrid from "../components/NFT/NFTGrid";
 import { NFT_COLLECTION_ADDRESS } from "../const/contractAddresses";
-import { NFT_COLLECTION_ADDRESS_2 } from "../const/contractAddresses";
 import tokenPageStyles from "../styles/Token.module.css";
 import { NFT as NFTType } from "@thirdweb-dev/sdk";
 import SaleInfo from "../components/SaleInfo/SaleInfo";
@@ -16,7 +15,6 @@ import SaleInfo from "../components/SaleInfo/SaleInfo";
 export default function Sell() {
   // Load all of the NFTs from the NFT Collection
   const { contract } = useContract(NFT_COLLECTION_ADDRESS);
-  const { contract } = useContract(NFT_COLLECTION_ADDRESS_2);
   const address = useAddress();
   const { data, isLoading } = useOwnedNFTs(contract, address);
 
