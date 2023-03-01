1bac109b8c34c9eaa58b795e1f7ad060cb2a8d2b
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2023-02-28 22:55:54 -0500

    Update contractAddresses.ts

--- const/contractAddresses.ts
+++ const/contractAddresses.ts
@@ -2,7 +2,7 @@
 
 // 1. Set up the network your smart contracts are deployed to.
 // First, import the chain from the package, then set the NETWORK variable to the chain.
-import { Mumbai } from "@thirdweb-dev/chains";
+import { Goerli } from "@thirdweb-dev/chains";
 export const NETWORK = Goerli;
 
 // 2. The address of the marketplace V3 smart contract.
